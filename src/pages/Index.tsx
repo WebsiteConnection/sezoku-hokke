54cc91efc6572383b9ade4eaee8b4217a949ed11
WebsiteConnection <jrtate1947@gmail.com>
2026-03-24 17:08:16 +0900

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,12 +1,19 @@
 import HeroSectionJa from "@/components/HeroSectionJa";
-import ResourcesSectionJa from "@/components/ResourcesSectionJa"; // Changed to Ja
-import FooterSectionJa from "@/components/FooterSectionJa";       // Changed to Ja
+import ResourcesSectionJa from "@/components/ResourcesSectionJa";
+import FooterSectionJa from "@/components/FooterSectionJa";
 
 const Index = () => {
   return (
     <div className="min-h-screen bg-background">
+      {/* The Hook */}
       <HeroSectionJa />
-      <ResourcesSectionJa />
+      
+      {/* The Evidence - Now directly on the home page */}
+      <div id="resources" className="border-t border-border/50">
+        <ResourcesSectionJa />
+      </div>
+
+      {/* The Footer */}
       <FooterSectionJa />
     </div>
   );

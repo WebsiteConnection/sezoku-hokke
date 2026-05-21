54fe85d99e39c05e44e4008554066a9dc05fa481
WebsiteConnection <jrtate1947@gmail.com>
2026-05-21 22:33:16 +0900

    Update ResourcesSectionJa.tsx

--- src/components/ResourcesSectionJa.tsx
+++ src/components/ResourcesSectionJa.tsx
@@ -29,7 +29,7 @@ const resources = [
   {
     title: "世俗仏教ネットワークの記事",
     description: "本教えが他の形態の世俗仏教とどのように異なるのかに焦点を当てています。",
-    icon: Globe,
+    icon: BookOpen,
     url: "https://drive.google.com/file/d/1EbT7dTw_JnYhhgHWqYkxFkElyvI42bVW/view?usp=drive_link"
   },
   {
